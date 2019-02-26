d1c2939651fad56ea4d175857dc959fca7dc2ad6
ScanonA <steven24.sca@gmail.com>
2019-02-25 22:55:55 -0700

    pushing to test in ucalgary servers

---
+++ seng300/Reviewer.java
@@ -0,0 +1,9 @@
+//import java.io.Exception;
+import java.io.BufferReader;
+
+
+public class Reviewer {
+	public static void main (String[] args)	 {//throws IOException {
+		//Process p = Runtime.getRuntime().exec("cmd /c ls .");
+	}
+}
